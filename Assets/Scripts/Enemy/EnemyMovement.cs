96a5eaffe0e2f782e90501456d912f1d15dea2e8
ArianaChaves <navi.ariana99@gmail.com>
2025-04-17 18:38:37 -0300

    feat: Animation constants. Enemy movement update.  Enemy Spawner now spawns zombie instead of capsule

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -2,29 +2,78 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    public enum EnemyState
+    {
+        Idle,
+        Moving,
+        Attacking
+    }
+
     [SerializeField] private float moveSpeed = 3.0f;
+
+    public EnemyState currentState = EnemyState.Idle; // Initial state
+    [SerializeField] private Animator animator;
 
     private Vector3 _targetDestination;
     private bool _hasTarget = false;
 
     void Update()
     {
+        switch (currentState)
+        {
+            case EnemyState.Idle:
+                UpdateIdle();
+                break;
+            case EnemyState.Moving:
+                UpdateMoving();
+                break;
+            case EnemyState.Attacking:
+                UpdateAttacking();
+                break;
+        }
+    }
+
+    private void UpdateIdle()
+    {
+        // Idle
+
+        if (_hasTarget)
+        {
+            currentState = EnemyState.Moving; // Transition to moving if we have a target.
+        }
+    }
+
+    private void UpdateMoving()
+    {
         if (_hasTarget)
         {
             Vector3 direction = (_targetDestination - transform.position).normalized;
 
-            transform.position += direction * moveSpeed * Time.deltaTime;
+            transform.position +=  moveSpeed * Time.deltaTime * direction;
 
             if (Vector3.Distance(transform.position, _targetDestination) < 0.1f)
             {
                 _hasTarget = false;
+                currentState = EnemyState.Idle;
             }
         }
+        else
+        {
+            currentState = EnemyState.Idle;
+        }
+    }
+
+    private void UpdateAttacking()
+    {
+        // Attack
+        //animator.Play(AnimationConstants.EnemyAnimations["Attack"]);
+        currentState = EnemyState.Idle; //Temporary, so it doesn't get stucked
     }
 
     public void SetDestination(Vector3 destination)
     {
         _targetDestination = destination;
         _hasTarget = true;
+        currentState = EnemyState.Moving;
     }
 }
